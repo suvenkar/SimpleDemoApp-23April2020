cbcfbafea23bcbc299c45ad59a463fabc69b3b83
suvenkar <suvenduitc@gmail.com>
2020-03-09 17:24:47 +0000

    V2

--- src/test/java/com/demo/SimpleDemoAppApplicationTests.java
+++ src/test/java/com/demo/SimpleDemoAppApplicationTests.java
@@ -2,7 +2,7 @@ package com.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//test
 @SpringBootTest
 class SimpleDemoAppApplicationTests {
 
