61613ba0b4888f97bdd9630ec73a29bc20d4016c
suvenkar <suvenduitc@gmail.com>
2020-04-23 16:19:15 +0000

    Renamed Spring boot application

--- src/main/java/com/demo/SimpleDemoApp.java
+++ src/main/java/com/demo/SimpleDemoApp.java
@@ -4,15 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//Test
 @SpringBootApplication
-public class SimpleDemoAppApplication extends SpringBootServletInitializer {
+//public class SimpleDemoAppApplication extends SpringBootServletInitializer {
+public class SimpleDemoApp extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleDemoAppApplication.class, args);
+		SpringApplication.run(SimpleDemoApp.class, args);
 	}
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(SimpleDemoAppApplication.class);
+		return builder.sources(SimpleDemoApp.class);
 	}
 }
